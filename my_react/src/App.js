61482cf2d96348a78e064bde91b4bdd51634382f
Derek Scace <derek.scace@gmail.com>
2018-06-02 23:49:14 -0700

    Add files via upload

--- my_react/src/App.js
+++ my_react/src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  getDayName(number) {
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',    'Thursday', 'Friday', 'Saturday'];
+    return days[number];
+  }
+
   render() {
-    return (
-      <div>Hello World</div>
-    );
+    // Get current date
+    let today = new Date().getDay();
+    // If it is weekend
+    if (today === 6 || today === 0)
+      return <div>Today is {this.getDayName(today)} - You can watch TV all day today</div>;
+    // If it is weekday
+    return <div>Today is {this.getDayName(today)} - You should go to work today</div>;
   }
 }
 
