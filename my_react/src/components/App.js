921729eb31fceea56c3531be43944093d3272875
dscace <derek.scace@gmail.com>
2018-06-10 21:40:17 -0700

    put current code on develop branch

---
+++ my_react/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import DayChecker from './DayChecker';
+import Header from './Header';
+
+class App extends Component {
+  render() {
+    return [
+      <Header />,
+      <DayChecker />
+    ];
+  }
+}
+
+export default App;
